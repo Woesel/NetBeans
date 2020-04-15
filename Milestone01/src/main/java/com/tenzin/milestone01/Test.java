39a525ae1f7d7a0bff681e229cd555bdc6527c1c
Tenzin Woesel <tenz_woesel@yahoo.com>
2020-04-15 10:57:51 -0400

    Testing

---
+++ Milestone01/src/main/java/com/tenzin/milestone01/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tenzin.milestone01;
+
+/**
+ *
+ * @author Tenzin Woesel
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Testing NetBeans and GitHub");
+    }
+}
